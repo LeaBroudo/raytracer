98a87bfa60c88ec2010f5860a02d7dcd486c324a
Lea Broudo <47679030+LeaBroudo@users.noreply.github.com>
2019-11-05 09:57:27 -0500

    Working, but camera position is off

--- camera.h
+++ camera.h
@@ -1,25 +1,44 @@
 #ifndef CAMERAH
 #define CAMERAH
 #include "ray.h"
+#include "random.h"
 
 class camera {
 public:
-    camera() {
-        ll_corner = vec3(-2.0, -1.0, 1.0);
-        horiz = vec3(4.0, 0.0, 0.0);
-        vert = vec3(0.0, 2.0, 0.0);
-        orig = vec3(0.0, 0.0, 0.0);
+    camera(vec3 og, vec3 visTo, vec3 ogUp, float vfov, float aspect, float aperture, float focus) {
+        
+        //Get ratios
+        float angle = (M_PI/180) * vfov;
+        float height_mid = tan(angle/2);
+        float width_mid = aspect * height_mid;
+        lens_rad = aperture/2;
+
+        //vectors
+        vec3 w = unit_vector(og - visTo);
+        vec3 u = unit_vector(cross(ogUp, w));
+        vec3 v = cross(w,u);
+        
+        //Create Dimensions
+        orig = og;
+        ll_corner = orig - (width_mid * u * focus) - (height_mid * v * focus) - (focus * w);
+        horiz = 2 * width_mid * u;
+        vert = 2 * height_mid * v;
     }
 
     vec3 orig;
     vec3 ll_corner;
-    vec3 horiz;
-    vec3 vert;    
+    vec3 horiz, vert;
+    vec3 u, v, w;  
+    float lens_rad;
     
-    ray get_ray(float u, float v) {
-        vec3 A = orig;
-        //vec3 B = ll_corner + u*horiz + v*vert - orig;
-        vec3 B = ll_corner + u*horiz + v*vert;
+    ray get_ray(float s, float t) {
+        
+        vec3 rd = lens_rad * rand_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+        
+        vec3 A = orig + offset;
+        vec3 B = ll_corner + s*horiz + t*vert - orig - offset;
+        
         return ray(A, B);
     }
 
--- mainOutput.cc
+++ mainOutput.cc
@@ -8,23 +8,45 @@
 #include "random.h"
 
 vec3 color(const ray& r, surface *world, int depth);
-//float hit_sphere(const vec3& center, float radius, const ray& r);
+surface *random_scene();
 
 int main() {
-	int xPos = 400;
-	int yPos = 200;
-    int sPos = 100;
+	int xPos = 300;
+	int yPos = 150;
+    int sPos = 10;
 
 	std::cout << "P3\n" << xPos << " " << yPos << "\n255\n";
     
     //Spheres
-    surface *list[2];
-    list[0] = new sphere(vec3(0,0,1), 0.5, new lambert(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new metal(vec3(0.8, 0.6, 0.2)));
-    surface *world = new surfaceList(list, 2);
+    ///*
+    surface *list[4];
+    list[0] = new sphere(vec3(0,0,1), .5, new lambert(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(1,0,1), .5, new metal(vec3(0.0, 0.3, 0.3), 1.0));
+    list[2] = new sphere(vec3(0,-100.5,-1), 100, new metal(vec3(0.8, 0.6, 0.2), 0.01));
+    list[3] = new sphere(vec3(-1,0,1), .5, new dielectric(1.5));
+    surface *world = new surfaceList(list, 4);
+    //*/
+    /*
+    surface *list[3];
+    list[0] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[1] = new sphere(vec3(-4, 1, 0), 1.0, new lambert(vec3(0.4, 0.2, 0.1)));
+    list[2] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    surface *world = new surfaceList(list, 3);
+    */
+    //surface *world = random_scene();
 
     //Camera
-    camera cam;
+
+    vec3 og(1,3,2);
+    vec3 dest(0,-1,0);
+    //vec3 og(4,12,8);
+    //vec3 dest(0,-1,1);
+    vec3 up = vec3(0,1,0);
+    float focus_dist = 10;
+    float aperture = 0.1;
+
+    camera cam(og, dest, up, 20, float(xPos)/float(yPos), aperture, focus_dist);
+
     
     for (int i = yPos-1; i >= 0; i--) {
 		for (int j = 0; j < xPos; j++) {
@@ -76,23 +98,42 @@ vec3 color(const ray& r, surface *world, int depth) {
     
 }
 
-/*
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-    
-    vec3 AC = r.origin() - center;
-    vec3 B = r.direction();
-
-    // (t^2)*dot(B,B) + 2t*dot(B, A-C) + dot(A-C, A-C) - R^2 = 0
-    //positive: two solutions, negative: no real solutions, zero:one real solution
-    float a = dot(B, B);
-    float b = 2.0 * dot(B, AC);
-    float c = dot(AC, AC) - pow(radius, 2.0);
-    float discriminant = pow(b, 2.0) - 4*a*c;
-    
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-b - sqrt(discriminant)) / (2.0*a);
+surface *random_scene() {
+    int n = 500;
+    surface **list = new surface*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambert(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    /*
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = get_rand();
+            vec3 center(a+0.9*get_rand(),0.2,b+0.9*get_rand());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2,
+                        new lambert(vec3(get_rand()*get_rand(),
+                                            get_rand()*get_rand(),
+                                            get_rand()*get_rand())
+                        )
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5*(1 + get_rand()),
+                                           0.5*(1 + get_rand()),
+                                           0.5*(1 + get_rand())),
+                                      0.5*get_rand()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
     }
+    */
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambert(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new surfaceList(list,i);
 }
-*/
